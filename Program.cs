3a492ae72dd1374ab39b248e674b1899a0e03710
= <martyofmca@gmail.coms>
2023-01-17 21:39:16 +0000

    feat: add feature to guess country
    add the feature to display a list of countries on the screen for the user to predict
    then we take the user's input and determine whether they are correct or not.
    
    Closes: #3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] countries = new string[] { "Ghana", "Togo", "Mali", "Nigeria", "Rwanda" };
+        int randomNumber;
+
+        randomNumber = new Random().Next(5);
+        string generatedCountry = countries[randomNumber];
+
+        Console.WriteLine("Ghana - Togo - Mali - Nigeria - Rwanda");
+        Console.Write("Guess a country: ");
+        string userOption = Console.ReadLine();
+        if(userOption == generatedCountry)
+        {
+            Console.WriteLine("Yay! You got it!");
+        }
+        else
+        {
+            Console.Write("Bummer! You had it wrong. ");
+            Console.Write("The correct option was ");
+            Console.WriteLine(generatedCountry);
+        }
+    }
+}
