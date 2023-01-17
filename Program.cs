c2e04266d0fec733b273c5e675c7484538ed9fd4
= <martyofmca@gmail.coms>
2023-01-17 21:51:58 +0000

    feat: add feature to guess fruit
    add the feature to display a list of fruit on the screen for the user to predict
    then we take the user's input and determine whether they are correct or not. The
    system asks the user's name to welcome them to the system then confirms whether
    the user wants to play fruit or country.
    
    Closes: #4

--- Program.cs
+++ Program.cs
@@ -2,16 +2,40 @@
 {
     private static void Main(string[] args)
     {
-        string[] countries = new string[] { "Ghana", "Togo", "Mali", "Nigeria", "Rwanda" };
+        string username;
+        Console.Write("What's your name: ");
+        username = Console.ReadLine();
+        string userOption;
+        Console.Write("Do you wish to play fruit (f) or country (c)? Press f or c: ");
+        userOption = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.Write("Welcome ");
+        Console.WriteLine(username);
+        Console.WriteLine();
+
         int randomNumber;
-
         randomNumber = new Random().Next(5);
-        string generatedCountry = countries[randomNumber];
+        string generatedItem = "nothing";
 
-        Console.WriteLine("Ghana - Togo - Mali - Nigeria - Rwanda");
-        Console.Write("Guess a country: ");
-        string userOption = Console.ReadLine();
-        if(userOption == generatedCountry)
+        if (userOption == "c")
+        {
+            string[] countries = new string[] { "Ghana", "Togo", "Mali", "Nigeria", "Rwanda" };
+            generatedItem = countries[randomNumber];
+
+            Console.WriteLine("Ghana - Togo - Mali - Nigeria - Rwanda");
+            Console.Write("Guess a country: ");
+        }
+        else if(userOption == "f")
+        {
+            string[] fruits = new string[] { "Pawpaw", "Orange", "Mango", "Cashew", "Pineapple" };
+            generatedItem = fruits[randomNumber];
+
+            Console.WriteLine("Pawpaw - Orange - Mango - Cashew - Pineapple");
+            Console.Write("Guess a fruit: ");
+        }
+        userOption = Console.ReadLine();
+        if (userOption == generatedItem)
         {
             Console.WriteLine("Yay! You got it!");
         }
@@ -19,7 +43,7 @@
         {
             Console.Write("Bummer! You had it wrong. ");
             Console.Write("The correct option was ");
-            Console.WriteLine(generatedCountry);
+            Console.WriteLine(generatedItem);
         }
     }
 }
